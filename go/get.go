ef7da269d4d9d652ebdac2376ec47fd4346e96e5
Zoey <zoey.spessanha@zeetech.io>
2021-05-18 13:09:42 -0300

    added go to the min README and simple get request
    fix go url snippet

---
+++ go/get.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := "you_endpoint"
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(string(body))
+}
